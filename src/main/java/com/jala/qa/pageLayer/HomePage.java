34e30ce0b3d51485823eb8fcc62e975a6bd36331
Your NameDeepakHaraleQA94 <haraledeepak6.com>
2025-01-28 20:30:59 +0530

    home page validation partially completed

--- src/main/java/com/jala/qa/pageLayer/HomePage.java
+++ src/main/java/com/jala/qa/pageLayer/HomePage.java
@@ -1,5 +1,31 @@
 package com.jala.qa.pageLayer;
 
-public class HomePage {
+import java.awt.print.PageFormat;
+import java.io.IOException;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.jala.qa.parentLayer.TestBase;
+
+public class HomePage extends TestBase{
+
+	
+	public HomePage() throws IOException {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(linkText = "Employee")
+	WebElement emplyeeTab;
+
+	@FindBy(linkText = "Create")
+	WebElement createTab;
+	
+	public void clickOnCreateTab() {
+		emplyeeTab.click();
+		createTab.click();
+	}
+	
+	
 }
